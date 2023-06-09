5a4e898e492fe8d4b8137260fd189aa24370ed29
HowLemon <77616267+HowLemon@users.noreply.github.com>
2023-06-09 12:22:39 +0800

    asset prefix fix

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = { 
     output: 'export',
-    assetPrefix: isProd ? 'portfolio' : undefined
+    assetPrefix: isProd ? '/portfolio' : undefined
 }
 
 module.exports = nextConfig
