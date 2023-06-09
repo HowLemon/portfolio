ae4c639fa55df9f50c2d6f1662c81adb0c58dc3c
HowLemon <77616267+HowLemon@users.noreply.github.com>
2023-06-09 12:20:10 +0800

    added asset prefix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {output: 'export'}
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = { 
+    output: 'export',
+    assetPrefix: isProd ? 'portfolio' : undefined
+}
 
 module.exports = nextConfig
